2f7b474bdf0bd8ed555e2bee24376828f0de5ddb
Pedro Pinto <pedro-esteves-pinto@users.noreply.github.com>
2022-12-05 08:11:25 -0500

    init

--- init.el
+++ init.el
@@ -150,3 +150,16 @@
 
 ;; Show home by default
 (dired "~")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-minibuffer-history-key "M-p")
+ '(safe-local-variable-values '((bodas . 1))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
