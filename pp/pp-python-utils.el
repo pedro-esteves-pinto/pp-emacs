000e0d9cb6544a589f60e4e52f07195502616586
Pedro Pinto <pedro-esteves-pinto@users.noreply.github.com>
2022-12-26 16:07:45 -0500

    Python utils.

--- pp/pp-python-utils.el
+++ pp/pp-python-utils.el
@@ -1,1 +1,9 @@
 (provide 'pp-python-utils)
+
+
+(setq python-indent-guess-indent-offset-verbose nil) 
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (eglot-ensure)
+	    (company-mode)
+	    (display-line-numbers-mode)))
